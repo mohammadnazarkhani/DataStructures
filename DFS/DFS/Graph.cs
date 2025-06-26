dc19ddb5328d3be170986bc011573a5146725810
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-26 22:51:26 +0330

    Add initial DFS project structure and classes
    Created solution and project files for a new DFS application targeting .NET 8.0. Added core classes: `Dfs`, `Graph`, and `Node` with methods for graph management. Included a simple console output in `Program.cs`.

---
+++ DFS/DFS/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFS
+{
+    public class Graph
+    {
+        public List<Node> Nodes { get; set; } = new List<Node>();
+
+        public void AddNode(Node node)
+        {
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            Nodes.Add(node);
+        }
+
+        public void AddEdge(Node from, Node to)
+        {
+            if (from == null || to == null) throw new ArgumentNullException("Nodes cannot be null");
+            from.AddNeighbor(to);
+        }
+
+        public void ResetVisited()
+        {
+            foreach (var node in Nodes)
+            {
+                node.IsVisited = false;
+            }
+        }
+    }
+}
---
+++ DFS/DFS/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFS
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public List<Node> Neighbors { get; set; } = new List<Node>();
+        public bool IsVisited { get; set; }
+
+        public Node()
+        {
+        }
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public void AddNeighbor(Node neighbor)
+        {
+            if (neighbor == null) throw new ArgumentNullException(nameof(neighbor));
+            if (Neighbors.Contains(neighbor)) return;
+
+            Neighbors.Add(neighbor);
+        }
+    }
+}
