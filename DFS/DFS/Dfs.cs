0680d1dd046f749d413d411a5a07ea5a705464eb
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-27 06:02:32 +0330

    Add depth-first traversal methods to Dfs class
    This commit introduces a new `Traverse` method in the `Dfs` class for performing depth-first traversal of a graph from a specified starting node. It includes a recursive helper method `TraverseRecursive` to visit nodes and track visited nodes. A null-checking method `ThrowIfNull` is also added to validate inputs before traversal.

--- DFS/DFS/Dfs.cs
+++ DFS/DFS/Dfs.cs
@@ -8,5 +8,33 @@ namespace DFS
 {
     public class Dfs
     {
+        public List<Node> Traverse(Graph graph, Node startNode)
+        {
+            ThrowIfNull(graph);
+            ThrowIfNull(startNode);
+
+            graph.ResetVisited();
+
+            var visitedNodes = new List<Node>();
+            TraverseRecursive(startNode, visitedNodes);
+            return visitedNodes;
+        }
+
+        public void TraverseRecursive(Node current, List<Node> visitedNodes)
+        {
+            current.IsVisited = true;
+            visitedNodes.Add(current);
+
+            foreach (var neighbor in current.Neighbors)
+                if (!neighbor.IsVisited) TraverseRecursive(neighbor, visitedNodes);
+        }
+
+        private void ThrowIfNull(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+        }
     }
 }
