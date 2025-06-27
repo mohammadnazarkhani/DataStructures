7164ba39f0820122ca1ea4c468112fb4df488ea1
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-27 06:34:49 +0330

    Add DFS search functionality with documentation
    Implemented a new `Search` method in the `DFS` namespace of `Dfs.cs` for node searching using Depth-First Search. Introduced a private `SearchRecursive` helper method for the recursive logic. Added XML documentation comments to improve code clarity and maintainability.

--- DFS/DFS/Dfs.cs
+++ DFS/DFS/Dfs.cs
@@ -29,6 +29,44 @@ namespace DFS
                 if (!neighbor.IsVisited) TraverseRecursive(neighbor, visitedNodes);
         }
 
+        /// <summary>
+        /// Searches for a node in the graph using DFS.
+        /// </summary>
+        /// <param name="graph">Graph to search</param>
+        /// <param name="startNode">First node to start searching</param>
+        /// <param name="searchingCriteria">
+        /// A predicate function which takes in a instance of Node and returns true if the node matches the searching criteria.
+        /// </param>
+        /// <returns>
+        /// Returns the first node that matches the searching criteria, or null if no such node is found.
+        /// </returns>
+        public Node? Search(Graph graph, Node startNode, Func<Node, bool> searchingCriteria)
+        {
+            ThrowIfNull(graph);
+            ThrowIfNull(startNode);
+            ThrowIfNull(searchingCriteria);
+
+            graph.ResetVisited();
+
+            return SearchRecursive(startNode, searchingCriteria);
+        }
+
+        public Node? SearchRecursive(Node current, Func<Node, bool> searchingCriteria)
+        {
+            current.IsVisited = true;
+            if (searchingCriteria(current)) return current;
+
+            foreach (var neighbor in current.Neighbors)
+            {
+                if (!neighbor.IsVisited)
+                {
+                    var result = SearchRecursive(neighbor, searchingCriteria);
+                    if (result != null) return result;
+                }
+            }
+            return null;
+        }
+
         private void ThrowIfNull(object obj)
         {
             if (obj == null)
